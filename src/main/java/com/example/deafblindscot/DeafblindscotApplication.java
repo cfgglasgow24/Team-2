ab2abd278f80e41b17df9118b44f3da0542e5892
RY4N247 <bramwellryan@gmail.com>
2024-04-11 10:33:34 +0100

    inital setup

---
+++ src/main/java/com/example/deafblindscot/DeafblindscotApplication.java
@@ -0,0 +1,13 @@
+package com.example.deafblindscot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeafblindscotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeafblindscotApplication.class, args);
+    }
+
+}
