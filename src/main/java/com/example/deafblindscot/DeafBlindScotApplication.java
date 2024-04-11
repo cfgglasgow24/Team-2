a7f8d9438a3f644ddf4c04aec68a14ca275e5404
RY4N247 <bramwellryan@gmail.com>
2024-04-11 11:38:03 +0100

    initial routes setup

--- src/main/java/com/example/deafblindscot/DeafBlindScotApplication.java
+++ src/main/java/com/example/deafblindscot/DeafBlindScotApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DeafblindscotApplication {
+public class DeafBlindScotApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DeafblindscotApplication.class, args);
+        SpringApplication.run(DeafBlindScotApplication.class, args);
     }
 
 }
--- src/main/java/com/example/deafblindscot/DeafBlindScotController.java
+++ src/main/java/com/example/deafblindscot/DeafBlindScotController.java
@@ -1,4 +1,28 @@
 package com.example.deafblindscot;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
+@Controller
 public class DeafBlindScotController {
+
+    @GetMapping("home")
+    //@ResponseBody
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("learn")
+    //@ResponseBody
+    public String learn() {
+        return "learn";
+    }
+
+
 }
+
+
+
+
+
+
